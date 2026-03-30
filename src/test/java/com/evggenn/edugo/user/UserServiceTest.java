5b37c724b90df808b0ac65893d7bc7eff818073c
Evgeny Buchelnikov <gru554@yandex.ru>
2026-03-30 20:27:12 +1000

    feat(test): add unit tests for UserService

---
+++ src/test/java/com/evggenn/edugo/user/UserServiceTest.java
@@ -0,0 +1,149 @@
+package com.evggenn.edugo.user;
+
+import com.evggenn.edugo.exception.EmailAlreadyExistsException;
+import com.evggenn.edugo.exception.UserNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    private Role studentRole;
+
+    private User expectedUser;
+
+    @Mock
+    private UserRepository userRepo;
+
+    @Mock
+    private RoleRepository roleRepo;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        studentRole = Role.builder()
+                .id(1L)
+                .name(Role.STUDENT)
+                .build();
+
+        expectedUser = User.builder()
+                .id(1L)
+                .email("test@test.com")
+                .password("hash_password")
+                .firstName("Vasia")
+                .lastName("Vasilkov")
+                .middleName("Zagagulivich")
+                .build();
+
+        expectedUser.getRoles().add(studentRole);
+    }
+
+    @Test
+    public void createUser_shouldReturnUser_whenEmailNotExists() {
+
+        when(userRepo.existsByEmail("test@test.com")).thenReturn(false);
+
+        when(passwordEncoder.encode("password")).thenReturn("hash_password");
+
+        when(roleRepo.findByName(Role.STUDENT)).thenReturn(Optional.of(studentRole));
+
+        when(userRepo.save(any(User.class))).thenReturn(expectedUser);
+
+        User createdUser = userService.createUser(
+                "test@test.com",
+                "password",
+                "Vasia",
+                "Vasilkov",
+                "Zagagulivich"
+        );
+
+        assertThat(createdUser.getId()).isEqualTo(expectedUser.getId());
+        assertThat(createdUser.getEmail()).isEqualTo("test@test.com");
+        assertThat(createdUser.getFirstName()).isEqualTo("Vasia");
+        assertThat(createdUser.getPassword()).isEqualTo("hash_password");
+
+        verify(userRepo).existsByEmail("test@test.com");
+        verify(passwordEncoder).encode("password");
+        verify(roleRepo).findByName(Role.STUDENT);
+        verify(userRepo).save(any(User.class));
+
+    }
+    @Test
+    public void createUser_whenEmailExists_throwsEmailAlreadyExistsException() {
+
+        when(userRepo.existsByEmail("test@test.com")).thenReturn(true);
+
+        Throwable thrown = assertThrows(EmailAlreadyExistsException.class, () -> {
+            userService.createUser(
+                    "test@test.com",
+                    "password",
+                    "Vasia",
+                    "Vasilkov",
+                    "Zagagulivich"
+            );
+        });
+
+        assertEquals("Email already exists: test@test.com", thrown.getMessage());
+    }
+
+    @Test
+    public void createUser_whenRoleNotExists_throwsIllegalStateException() {
+
+        when(userRepo.existsByEmail("test@test.com")).thenReturn(false);
+        when(roleRepo.findByName(Role.STUDENT)).thenReturn(Optional.empty());
+
+        Throwable thrown = assertThrows(IllegalStateException.class, () -> {
+            userService.createUser(
+                    "test@test.com",
+                    "password",
+                    "Vasia",
+                    "Vasilkov",
+                    "Zagagulivich"
+            );
+        });
+
+        assertEquals("Role STUDENT not found", thrown.getMessage());
+    }
+
+    @Test
+    public void findByEmailWithRolesOrThrow_whenUserFound_returnsUser() {
+
+        when(userRepo.findByEmailWithRoles(expectedUser.getEmail())).thenReturn(Optional.of(expectedUser));
+
+        User createdUser = userService.findByEmailWithRolesOrThrow("test@test.com");
+
+        assertThat(createdUser.getRoles()).hasSize(1);
+        assertThat(createdUser.getRoles()).extracting(Role::getName).containsExactlyInAnyOrder(Role.STUDENT);
+
+    }
+
+    @Test
+    public void findByEmailWithRolesOrThrow_whenUserNotFound_throwsException() {
+
+        when(userRepo.findByEmailWithRoles(expectedUser.getEmail())).thenReturn(Optional.empty());
+
+        Throwable thrown = assertThrows(UserNotFoundException.class, () -> {
+            userService.findByEmailWithRolesOrThrow(expectedUser.getEmail());
+        });
+
+        assertEquals("User not found with email: " + expectedUser.getEmail(), thrown.getMessage());
+    }
+}
