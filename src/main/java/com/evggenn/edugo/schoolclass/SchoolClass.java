a5fc646b15efba7bf95d3a708991454b3c6337b8
Evgeny Buchelnikov <gru554@yandex.ru>
2026-04-05 13:36:44 +1000

    feat(schoolclass): add SchoolClass entity

---
+++ src/main/java/com/evggenn/edugo/schoolclass/SchoolClass.java
@@ -0,0 +1,45 @@
+package com.evggenn.edugo.schoolclass;
+
+import com.evggenn.edugo.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "classes")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Setter
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class SchoolClass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "academic_year", nullable = false, length = 9)
+    private String academicYear;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id")
+    private User teacher;
+
+    @ManyToMany
+    @JoinTable(
+            name = "student_classes",
+            joinColumns = @JoinColumn(name = "class_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private Set<User> students = new HashSet<>();
+
+    public SchoolClass(String name, String academicYear) {
+        this.name = name;
+        this.academicYear = academicYear;
+    }
+}
