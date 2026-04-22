cd23ec800539a195231c27717554ff0952545c19
Evgeny Buchelnikov <gru554@yandex.ru>
2026-04-22 22:35:14 +1000

    feat(period): add Repository with core methods

--- src/main/java/com/evggenn/edugo/period/PeriodRepository.java
+++ src/main/java/com/evggenn/edugo/period/PeriodRepository.java
@@ -5,12 +5,29 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface PeriodRepository extends JpaRepository<Period, Long> {
 
     boolean existsByNameAndAcademicYear(String name, String academicYear);
 
+    Optional<Period> findByIdAndAcademicYear(Long id, String academicYear);
+
     List<Period> findAllByAcademicYear(String academicYear);
+
+    @Query("""
+    SELECT COUNT(p) > 0 FROM Period p
+    WHERE p.academicYear = :academicYear
+    AND p.startDate <= :newEnd
+    AND p.endDate >= :newStart
+    AND p.id <> :id
+""")
+    boolean existsOverlappingPeriodExcludingId(
+            String academicYear,
+            LocalDate newStart,
+            LocalDate newEnd,
+            Long id
+    );
 
     @Query("""
         SELECT COUNT(p) > 0 FROM Period p
