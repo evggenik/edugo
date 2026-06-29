d3f43a554440b2ed9135c905ae18522d8965e9c8
Evgeny Buchelnikov <gru554@yandex.ru>
2026-06-29 12:00:59 +1000

    Add HomeworkRepository
    - Add existsByLessonId, findByLessonId, findByIdWithLesson

---
+++ src/main/java/com/evggenn/edugo/homework/HomeworkRepository.java
@@ -0,0 +1,15 @@
+package com.evggenn.edugo.homework;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface HomeworkRepository extends JpaRepository<Homework, Long> {
+    boolean existsByLessonId(Long lessonId);
+
+    Optional<Homework> findByLessonId(Long lessonId);
+
+    @Query("SELECT h FROM Homework h JOIN FETCH h.lesson WHERE h.id = :id")
+    Optional<Homework> findByIdWithLesson(Long id);
+}
